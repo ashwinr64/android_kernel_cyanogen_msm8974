83c56166bcba9f9732ee64a297a8c3932ca3016f
Ashwin Ramesh <ashwinr64@gmail.com>
2018-06-18 23:25:41 +0530

    ASoc: codecs: wcd9xxx-mbhc: Import k9 changes
    Change-Id: Ida12046c12c926eea7a7912a3d5ac2dfa4bc2d7f

--- sound/soc/codecs/wcd9xxx-mbhc.c
+++ sound/soc/codecs/wcd9xxx-mbhc.c
@@ -71,7 +71,11 @@
 #define SWCH_IRQ_DEBOUNCE_TIME_US 5000
 #define BTN_RELEASE_DEBOUNCE_TIME_MS 25
 
+#ifdef CONFIG_MACH_SHENQI_K9
+#define GND_MIC_SWAP_THRESHOLD 4
+#else
 #define GND_MIC_SWAP_THRESHOLD 2
+#endif
 #define OCP_ATTEMPT 1
 
 #define FW_READ_ATTEMPTS 15
@@ -86,7 +90,11 @@
 #define DEFAULT_DCE_WAIT 60000
 #define DEFAULT_STA_WAIT 5000
 
+#ifdef CONFIG_MACH_SHENQI_K9
+#define VDDIO_MICBIAS_MV 2700
+#else
 #define VDDIO_MICBIAS_MV 1800
+#endif
 
 #define WCD9XXX_MICBIAS_PULLDOWN_SETTLE_US 5000
 
@@ -120,10 +128,19 @@
 /* RX_HPH_CNP_WG_TIME increases by 0.24ms */
 #define WCD9XXX_WG_TIME_FACTOR_US	240
 
+#ifdef CONFIG_MACH_SHENQI_K9
+#define WCD9XXX_V_CS_HS_MAX 2500
+#define WCD9XXX_V_CS_NO_MIC 10
+#else
 #define WCD9XXX_V_CS_HS_MAX 500
 #define WCD9XXX_V_CS_NO_MIC 5
+#endif
 #define WCD9XXX_MB_MEAS_DELTA_MAX_MV 80
+#ifdef CONFIG_MACH_SHENQI_K9
+#define WCD9XXX_CS_MEAS_DELTA_MAX_MV 14
+#else
 #define WCD9XXX_CS_MEAS_DELTA_MAX_MV 12
+#endif
 
 static int impedance_detect_en;
 module_param(impedance_detect_en, int,
@@ -812,12 +829,22 @@ static void wcd9xxx_insert_detect_setup(struct wcd9xxx_mbhc *mbhc, bool ins)
 		 ins ? "insert" : "removal");
 	/* Disable detection to avoid glitch */
 	snd_soc_update_bits(mbhc->codec, WCD9XXX_A_MBHC_INSERT_DETECT, 1, 0);
+#ifdef CONFIG_MACH_SHENQI_K9
+	/* Use outer pull-up. */
+	if (mbhc->mbhc_cfg->gpio_level_insert)
+		snd_soc_write(mbhc->codec, WCD9XXX_A_MBHC_INSERT_DETECT,
+			      (0x18 | (ins ? (1 << 1) : 0)));
+	else
+		snd_soc_write(mbhc->codec, WCD9XXX_A_MBHC_INSERT_DETECT,
+			      (0x1C | (ins ? (1 << 1) : 0)));
+#else
 	if (mbhc->mbhc_cfg->gpio_level_insert)
 		snd_soc_write(mbhc->codec, WCD9XXX_A_MBHC_INSERT_DETECT,
 			      (0x68 | (ins ? (1 << 1) : 0)));
 	else
 		snd_soc_write(mbhc->codec, WCD9XXX_A_MBHC_INSERT_DETECT,
 			      (0x6C | (ins ? (1 << 1) : 0)));
+#endif
 	/* Re-enable detection */
 	snd_soc_update_bits(mbhc->codec, WCD9XXX_A_MBHC_INSERT_DETECT, 1, 1);
 }
@@ -1443,6 +1470,7 @@ wcd9xxx_cs_find_plug_type(struct wcd9xxx_mbhc *mbhc,
 			if (!minv || minv > d->_vdces)
 				minv = d->_vdces;
 		}
+#ifndef CONFIG_MACH_SHENQI_K9
 		if ((!d->mic_bias &&
 		    (d->_vdces >= WCD9XXX_CS_MEAS_INVALD_RANGE_LOW_MV &&
 		     d->_vdces <= WCD9XXX_CS_MEAS_INVALD_RANGE_HIGH_MV)) ||
@@ -1453,6 +1481,7 @@ wcd9xxx_cs_find_plug_type(struct wcd9xxx_mbhc *mbhc,
 			type = PLUG_TYPE_INVALID;
 			goto exit;
 		}
+#endif
 	}
 
 	delta_thr = ((highhph_cnt == sz) || highhph) ?
@@ -1530,6 +1559,7 @@ wcd9xxx_cs_find_plug_type(struct wcd9xxx_mbhc *mbhc,
 		goto exit;
 	}
 
+#ifndef CONFIG_MACH_SHENQI_K9
 	if (!(event_state & (1UL << MBHC_EVENT_PA_HPHL))) {
 		if (((type == PLUG_TYPE_HEADSET ||
 		      type == PLUG_TYPE_HEADPHONE) && ch != sz)) {
@@ -1538,6 +1568,7 @@ wcd9xxx_cs_find_plug_type(struct wcd9xxx_mbhc *mbhc,
 			type = PLUG_TYPE_INVALID;
 		}
 	}
+#endif
 
 	if (type == PLUG_TYPE_HEADSET &&
 	    (mbhc->mbhc_cfg->micbias_enable_flags &
@@ -3090,6 +3121,22 @@ static void wcd9xxx_correct_swch_plug(struct work_struct *work)
 			}
 		} else if (plug_type == PLUG_TYPE_HEADPHONE) {
 			pr_debug("Good headphone detected, continue polling\n");
+#ifdef CONFIG_MACH_SHENQI_K9 /* For workground */
+			pt_gnd_mic_swap_cnt++;
+			if (mbhc->mbhc_cfg->swap_gnd_mic) {
+				/*
+				* if switch is toggled, check again,
+				* otherwise report unsupported plug
+				*/
+				if (pt_gnd_mic_swap_cnt < 3) {
+					printk(KERN_DEBUG "Try to swap GND MIC\n");
+					mbhc->mbhc_cfg->swap_gnd_mic(codec);
+					continue;
+				}
+				printk(KERN_DEBUG "%s: pt_gnd_mic_swap_cnt=%d\n",
+					__func__, pt_gnd_mic_swap_cnt);
+			}
+#endif
 			WCD9XXX_BCL_LOCK(mbhc->resmgr);
 			if (mbhc->mbhc_cfg->detect_extn_cable) {
 				if (mbhc->current_plug != plug_type)
@@ -3245,7 +3292,9 @@ static void wcd9xxx_swch_irq_handler(struct wcd9xxx_mbhc *mbhc)
 			wcd9xxx_report_plug(mbhc, 0, SND_JACK_HEADPHONE);
 			is_removed = true;
 		} else if (mbhc->current_plug == PLUG_TYPE_GND_MIC_SWAP) {
+#ifndef CONFIG_MACH_SHENQI_K9
 			wcd9xxx_report_plug(mbhc, 0, SND_JACK_UNSUPPORTED);
+#endif
 			is_removed = true;
 		} else if (mbhc->current_plug == PLUG_TYPE_HEADSET) {
 			wcd9xxx_pause_hs_polling(mbhc);
@@ -3393,6 +3442,30 @@ static int wcd9xxx_determine_button(const struct wcd9xxx_mbhc *mbhc,
 static int wcd9xxx_get_button_mask(const int btn)
 {
 	int mask = 0;
+#ifdef CONFIG_MACH_SHENQI_K9
+	switch (btn) {
+	case 0:
+	case 1:
+		mask = SND_JACK_BTN_0;
+		break;
+	case 2:
+		mask = SND_JACK_BTN_5;
+		break;
+	case 3:
+		mask = SND_JACK_BTN_0;
+		break;
+	case 4:
+		mask = SND_JACK_BTN_4;
+		break;
+	case 5:
+	case 6:
+		mask = SND_JACK_BTN_5;
+		break;
+	case 7:
+		mask = SND_JACK_BTN_7;
+		break;
+	}
+#else
 	switch (btn) {
 	case 0:
 		mask = SND_JACK_BTN_0;
@@ -3419,6 +3492,7 @@ static int wcd9xxx_get_button_mask(const int btn)
 		mask = SND_JACK_BTN_7;
 		break;
 	}
+#endif
 	return mask;
 }
 
@@ -4998,6 +5072,24 @@ int wcd9xxx_mbhc_init(struct wcd9xxx_mbhc *mbhc, struct wcd9xxx_resmgr *resmgr,
 			return ret;
 		}
 
+#ifdef CONFIG_MACH_SHENQI_K9
+		ret = snd_jack_set_key(mbhc->button_jack.jack,
+					   SND_JACK_BTN_5,
+					   KEY_VOLUMEUP);
+		if (ret) {
+			pr_err("%s: Failed to set code for btn-5\n",
+				__func__);
+			return ret;
+		}
+		ret = snd_jack_set_key(mbhc->button_jack.jack,
+				       SND_JACK_BTN_7,
+				       KEY_VOLUMEDOWN);
+		if (ret) {
+			pr_err("%s: Failed to set code for btn-0\n",
+				__func__);
+			return ret;
+		}
+#endif
 		INIT_DELAYED_WORK(&mbhc->mbhc_firmware_dwork,
 				  wcd9xxx_mbhc_fw_read);
 		INIT_DELAYED_WORK(&mbhc->mbhc_btn_dwork, wcd9xxx_btn_lpress_fn);
